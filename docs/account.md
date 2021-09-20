3336580a7d06de7f2eaa13e3d4ae536dce146701
Owen Voke <development@voke.dev>
2021-09-20 20:01:05 +0100

    feat(account.ens): add api

--- docs/account.md
+++ docs/account.md
@@ -11,3 +11,19 @@ $response = $client->account()->showByAddressOrEnsName(
     '0x3ab56c8a5e4b307a60b6a769b1c083ee165d6dd6'
 );
 ```
+
+### Get an ETH address from an ENS name
+
+```php
+$response = $client->account()->ensNameToAddress(
+    'voke.eth'
+);
+```
+
+### Get an ENS name from an ETH address
+
+```php
+$response = $client->account()->addressTpEnsName(
+    '0x3ab56c8a5e4b307a60b6a769b1c083ee165d6dd6'
+);
+```
--- src/Api/Account.php
+++ src/Api/Account.php
@@ -10,4 +10,16 @@ class Account extends AbstractApi
     {
         return $this->get("/actions/scan/{$addressOrEnsName}");
     }
+
+    public function ensNameToAddress(string $ensName): array
+    {
+        return $this->get('/actions/ens_resolve', [
+            'name' => $ensName,
+        ]);
+    }
+
+    public function addressToEnsName(string $address): array
+    {
+        return $this->get("/actions/ens_lookup/{$address}");
+    }
 }
