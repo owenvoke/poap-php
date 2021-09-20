7fe7239f7dc8b46a441981331e2736ee1d25f590
Owen Voke <development@voke.dev>
2021-09-20 11:23:04 +0100

    tests(event): add tests

---
+++ tests/Api/EventTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+use OwenVoke\POAP\Api\Event;
+
+beforeEach(fn() => $this->apiClass = Event::class);
+
+$singleEventData = [
+    "id" => 7811,
+    "fancy_id" => "pest-php-meetup-1-2021",
+    "name" => "Pest PHP Meetup #1",
+    "event_url" => "https://pestphp.com",
+    "image_url" => "https://storage.googleapis.com/poapmedia/pest-php-meetup-1-2021-logo-1631810734471.png",
+    "country" => "",
+    "city" => "",
+    "description" => "PEST Meetup #1: Testing Livewire with PEST & You know the REST, now it's time for Pest
+
+Schedule:
+6:00 PM - Intro
+
+6:05 PM - “Wired Pest - Testing Livewire with Pest” by @tio_jobs
+
+6:25 PM - “You know the REST, now it's time for Pest” by @DanSysAnalyst
+
+6:45 PM - After Party
+
+7:00 PM - End
+
+Pest is an elegant PHP Testing Framework with a focus on simplicity. It was carefully crafted to bring the joy of testing to PHP.
+
+https://youtu.be/q_8kRlAIyms",
+    "year" => 2021,
+    "start_date" => "22-Jun-2021",
+    "end_date" => "22-Jun-2021",
+    "expiry_date" => "19-Sep-2021",
+    "from_admin" => false,
+    "virtual_event" => true,
+    "event_template_id" => 0,
+    "event_host_id" => 0
+];
+
+it('should get an event by its id', function () use ($singleEventData) {
+    $api = $this->getApiMock();
+
+    $api->expects($this->once())
+        ->method('get')
+        ->with('/events/id/7811')
+        ->will($this->returnValue($singleEventData));
+
+    expect($api->show(7811))->toBe($singleEventData);
+});
+
+it('should get an event by its slug', function () use ($singleEventData) {
+    $api = $this->getApiMock();
+
+    $api->expects($this->once())
+        ->method('get')
+        ->with('/events/pest-php-meetup-1-2021')
+        ->will($this->returnValue($singleEventData));
+
+    expect($api->showBySlug('pest-php-meetup-1-2021'))->toBe($singleEventData);
+});
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,8 +2,13 @@
 
 declare(strict_types=1);
 
+use OwenVoke\POAP\Api\Event;
 use OwenVoke\POAP\Client;
 
 it('gets instances from the client', function () {
     $client = new Client();
+
+    // Retrieves Event instance
+    expect($client->event())->toBeInstanceOf(Event::class);
+    expect($client->events())->toBeInstanceOf(Event::class);
 });
